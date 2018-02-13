10b15c2b376217bdd3014f8367c47766c7c98a05
davidyshin <davidshin@ac.c4q.nyc>
2018-02-13 15:50:36 -0500

    fixed merge bugs

--- front-end/src/components/users/UserProfile.jsx
+++ front-end/src/components/users/UserProfile.jsx
@@ -1,59 +1,44 @@
-import React from 'react'
-import Modal from 'react-modal';
-import { Route, Link, Switch } from 'react-router-dom'
-import NewPost from "./NewPost"
-import "../../user-profile.css"
+import React from "react";
+import Modal from "react-modal";
+import { Route, Link, Switch } from "react-router-dom";
+import NewPost from "./NewPost";
+import "../../user-profile.css";
 
-
-   
 class UserProfile extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            modalIsOpen: false
-        }
-    }
-    toggleModal = () => {
-        let {modalIsOpen} = this.state
-        this.setState({
-            modalIsOpen: !modalIsOpen
-        })
-    }
+  constructor() {
+    super();
+    this.state = {
+      modalIsOpen: false
+    };
+  }
+  toggleModal = () => {
+    let { modalIsOpen } = this.state;
+    this.setState({
+      modalIsOpen: !modalIsOpen
+    });
+  };
+
   render() {
-      let {user, logout} = this.props
-      let {modalIsOpen} = this.state
+    let { user, logout } = this.props;
+    let { modalIsOpen } = this.state;
     return (
       <div className="header-bar">
-        <div >
-          <h1 className="header-title">Self-ish</h1>
-        
-        <button>View Feed</button>
-        <button>Edit Profile</button>
-        <Modal isOpen={modalIsOpen} contentLabel="New Post">
-          <NewPost toggleModal={this.toggleModal}/>
-        </Modal>
-
-        <button onClick={this.toggleModal} className="newPost">
-          <i class="fa fa-plus-square" />
-        </button>
-
-const UserProfile = ({ user, logout }) => {
-    console.log(user)
-    return (
         <div>
-            <header>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-                </header>
-            <h1>{user}!</h1>
-            <button>View Feed</button>
-            <Link to="/users/editprofile"><button>Edit Profile</button></Link>
-            <button className="addPost"><i class="fa fa-plus-square" ></i></button>
+          <h1 className="header-title">Self-ish!</h1>
+
+          <button>View Feed</button>
+          <Modal isOpen={modalIsOpen} contentLabel="New Post">
+            <NewPost toggleModal={this.toggleModal} />
+          </Modal>
+  
+          <button><Link to="/users/editprofile">Edit Profile</Link></button>
+            <button onClick={this.toggleModal} className="newPost">
+              <i class="fa fa-plus-square" />
+            </button>
             <button onClick={logout}>LOGOUT :(</button>
-            <br />
         </div>
       </div>
     );
   }
 }
-
 export default UserProfile;
--- front-end/src/components/users/Users.jsx
+++ front-end/src/components/users/Users.jsx
@@ -1,98 +1,83 @@
-import React from 'react'
-import { Route, Link, Switch } from 'react-router-dom'
-import NewUser from './NewUser'
-import LoginUser from './LoginUser'
-import EditProfile from './EditProfile'
-import UserProfile from './UserProfile'
-import axios from 'axios'
-
-
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import NewUser from "./NewUser";
+import LoginUser from "./LoginUser";
+import EditProfile from "./EditProfile";
+import UserProfile from "./UserProfile";
+import axios from "axios";
 
 class Users extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            user: '',
-            active: false,
-        }
-        this.allUsers = []
-    }
+  constructor() {
+    super();
+    this.state = {
+      user: "",
+      active: false
+    };
+    this.allUsers = [];
+  }
 
-
-    componentDidMount() {
-        const { user } = this.state
-        axios
-            .get('/users/getUser')
-            .then((res) => {
-                this.setState({
-                    user: res.data.user,
-                    active: true
-                })
-            })
-            .catch((err) => {
-                console.log(`errrr`, err)
-            })
-    }
-
-
-
-    UserFound = (user) => {
+  componentDidMount() {
+    const { user } = this.state;
+    axios
+      .get("/users/getUser")
+      .then(res => {
         this.setState({
-            user: user
-        })
-    }
+          user: res.data.user,
+          active: true
+        });
+      })
+      .catch(err => {
+        console.log(`errrr`, err);
+      });
+  }
 
-    isActive = () => {
+  UserFound = user => {
+    this.setState({
+      user: user
+    });
+  };
+
+  isActive = () => {
+    this.setState({
+      active: !this.state.active
+    });
+  };
+
+  logOut = () => {
+    axios
+      .get("/users/logout")
+      .then(res => {
         this.setState({
-            active: !this.state.active
-        })
+          active: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  renderProfile = () => {
+    console.log("HITTING");
+    const { active, user } = this.state;
+    if (active === false) {
+      return <LoginUser active={this.isActive} user={this.UserFound} />;
+    } else {
+      return <UserProfile user={user.username} logout={this.logOut} />;
     }
+  };
 
-    logOut = () => {
-        axios
-            .get('/users/logout')
-            .then((res) => {
-                this.setState({
-                    active: false
-                })
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }
-
-
-    renderProfile = () => {
-        console.log("HITTING")
-        const { active, user } = this.state
-        if (active === false) {
-            return (
-                <LoginUser active={this.isActive} user={this.UserFound} />
-            )
-        } else {
-            return (
-                <UserProfile user={user.username} logout={this.logOut} />
-            )
-        }
-
-    }
-
-   
-    render() {
-        console.log(`user`, this.state)
-        return (
-            
-            <div>
-                <Switch>
-                    <Route exact path='/' component={NewUser} />
-                    <Route exact path='/users/login' component={LoginUser} />
-                    <Route exact path='/users/editprofile' component={EditProfile} />
-
-                </Switch>
-            </div>
-        )
-    }
+  render() {
+    console.log(`user`, this.state);
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={NewUser} />
+          <Route exact path="/users/login" component={this.renderProfile} />
+          <Route exact path="/users/editprofile" component={EditProfile} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-
-export default Users
+export default Users;
