1a655cc71f4aee4302a6b60e823934c5ebc37657
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2018-02-14 17:30:16 -0500

    add seed data for database.

--- back-end/db/usersPostgres.sql
+++ back-end/db/usersPostgres.sql
@@ -33,4 +33,15 @@ CREATE TABLE follows (
   followee_id INTEGER REFERENCES users(ID)
 );
 
+INSERT INTO users VALUES (1, 'john@jazz.com', 'John Stockton', 'assistleader', 'password');
+INSERT INTO users VALUES (2, 'mailman@jazz.com', 'Karl Malone', 'mailman', 'password');
+INSERT INTO users VALUES (3, 'notarolemodel@suns.com', 'Charles Barkley', 'sircharles', 'password');
+INSERT INTO users VALUES (4, 'mrrobinson@spurs.com', 'David Robinson', 'admiral', 'password');
+INSERT INTO users VALUES (5, 'clyde@blazers.com', 'Clyde Drexler', 'theglide', 'password');
+INSERT INTO users VALUES (6, 'legend@celtics.com', 'Larry Bird', 'larrylegend', 'password');
+INSERT INTO users VALUES (7, 'lefty@warriors.com', 'Chris Mullin', 'luckylefty', 'password');
+INSERT INTO users VALUES (8, 'magic@lakers.com', 'Magic Johnson', 'magic', 'password');
+INSERT INTO users VALUES (9, 'pip@bulls.com', 'Scottie Pippen', 'pippenainteasy', 'password');
+INSERT INTO users VALUES (10, 'air@bulls.com', 'Michael Jordan', 'airjordan', 'password');
+
 
