e79b22fe1f84cb4ac52c6f864143c6b0763e633e
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2018-02-14 16:03:12 -0500

    create backend database for users, posts, likes, and follows.

--- back-end/db/usersPostgres.sql
+++ back-end/db/usersPostgres.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS userlist;
-CREATE DATABASE userlist;
+DROP DATABASE IF EXISTS selfish_users;
+CREATE DATABASE selfish_users;
 
-\c userlist;
+\c selfish_users;
 
 
 CREATE TABLE users (
@@ -9,9 +9,28 @@ CREATE TABLE users (
   email VARCHAR UNIQUE,
   full_name VARCHAR,
   username VARCHAR UNIQUE,
-  password_digest VARCHAR,
-  post VARCHAR (8000),
-  following VARCHAR,
-  followers VARCHAR,
-  likes INTEGER
+  password_digest VARCHAR
 );
+
+CREATE TABLE posts (
+  ID SERIAL PRIMARY KEY,
+  post_image VARCHAR,
+  caption VARCHAR,
+  user_id INTEGER REFERENCES users(ID),
+  dates VARCHAR
+);
+
+CREATE TABLE likes (
+  ID SERIAL PRIMARY KEY,
+  liker_id INTEGER REFERENCES users(ID),
+  post_id INTEGER REFERENCES posts(ID)
+);
+
+
+CREATE TABLE follows (
+  ID SERIAL PRIMARY KEY,
+  follower_id INTEGER REFERENCES users(ID),
+  followee_id INTEGER REFERENCES users(ID)
+);
+
+
