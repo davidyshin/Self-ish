a13292a0bf012d9de86bd2b6ab2c4ce0bb845254
davidyshin <davidshin@ac.c4q.nyc>
2018-02-15 00:26:15 -0500

    started skeleton on userfeed

--- front-end/src/components/users/Home.jsx
+++ front-end/src/components/users/Home.jsx
@@ -37,39 +37,41 @@ class Home extends React.Component {
     return (
       <div className="user-home">
         <div className="header-bar">
-          <div className="left-top">
-            <Link to="/">
-              <i class="fas fa-camera-retro" />
-              {"    "}Self-ish
-            </Link>
-          </div>
-          <div className="search-box">
-            <input
-              type="search"
-              name="search"
-              id="search"
-              placeholder="Search"
-            />
-          </div>
-          <div className="right-top">
-            <div className="user-buttons">
-              <Modal isOpen={modalIsOpen} contentLabel="New Post">
-                <NewPost toggleModal={this.toggleModal} />
-              </Modal>
+          <div className="header">
+            <div className="left-top">
+              <Link to="/">
+                <i class="fas fa-camera-retro" />
+                {"    "}Self-ish
+              </Link>
+            </div>
+            <div className="search-box">
+              <input
+                type="search"
+                name="search"
+                id="search"
+                placeholder="Search"
+              />
+            </div>
+            <div className="right-top">
+              <div className="user-buttons">
+                <Modal isOpen={modalIsOpen} contentLabel="New Post">
+                  <NewPost toggleModal={this.toggleModal} />
+                </Modal>
 
-              <button>
-                <Link to="/profile">
-                  <i class="far fa-user fa-2x" />
-                </Link>
-              </button>
+                <button>
+                  <Link to="/profile">
+                    <i class="far fa-user fa-2x" />
+                  </Link>
+                </button>
 
-              <button onClick={this.toggleModal} className="newPost">
-                <i class="fa fa-plus-square fa-2x" />
-              </button>
+                <button onClick={this.toggleModal} className="newPost">
+                  <i class="fa fa-plus-square fa-2x" />
+                </button>
 
-              <button onClick={logOut}>
-                <i class="fas fa-sign-out-alt fa-2x" />
-              </button>
+                <button onClick={logOut}>
+                  <i class="fas fa-sign-out-alt fa-2x" />
+                </button>
+              </div>
             </div>
           </div>
         </div>
