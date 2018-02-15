0aaae2143f3a45c414b4fa8f0fa3260ced0476cc
newton-brooks <newtonbrooks.ac.c4q.nyc>
2018-02-15 10:24:15 -0500

    almost done

--- back-end/db/usersPostgres.sql
+++ back-end/db/usersPostgres.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS userlist;
-CREATE DATABASE userlist;
+DROP DATABASE IF EXISTS selfish_users;
+CREATE DATABASE selfish_users;
 
-\c userlist;
+\c selfish_users;
 
 
 CREATE TABLE users (
@@ -9,9 +9,34 @@ CREATE TABLE users (
   email VARCHAR UNIQUE,
   full_name VARCHAR,
   username VARCHAR UNIQUE,
-  password_digest VARCHAR,
-  post VARCHAR (8000),
-  following VARCHAR,
-  followers VARCHAR,
-  likes INTEGER
+  password_digest VARCHAR
 );
+
+CREATE TABLE posts (
+  ID SERIAL PRIMARY KEY,
+  post_image VARCHAR,
+  caption VARCHAR,
+  user_id INTEGER REFERENCES users(ID),
+  dates VARCHAR
+);
+
+CREATE TABLE likes (
+  ID SERIAL PRIMARY KEY,
+  liker_id INTEGER REFERENCES users(ID),
+  post_id INTEGER REFERENCES posts(ID)
+);
+
+
+CREATE TABLE follows (
+  ID SERIAL PRIMARY KEY,
+  follower_id INTEGER REFERENCES users(ID),
+  followee_id INTEGER REFERENCES users(ID)
+);
+
+
+
+INSERT INTO users (email, full_name, username, password_digest)
+VALUES('google@example.com', 'newton', 'newton123', 'newt'),
+      ('yahoo@example.com', 'carolina', 'carolina456', 'carol'),
+      ('aol@example.com', 'eddie', 'eddie789', 'ed')
+
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -9,20 +9,46 @@ const { loginRequired } = require("../auth/helpers");
 
 
 /* GET users listing. */
+
+//User login/logout and registration ROUTES
 router.post('/new', db.registerUser);
 router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json(req.user);
 })
 router.get('/logout',loginRequired, db.logoutUser)
-router.get('/all', db.getAllUsers)
-router.get('/getUser', loginRequired, db.getUser)
-router.post('/addPost', loginRequired, db.addPost)
-router.patch('/updateLikes', loginRequired, db.updateLikes)
-router.get('/getUserLikes', loginRequired, db.getUserLikes)
-router.get('/getAllPost', loginRequired, db.getAllPost)
-router.get('/userPost', loginRequired, db.getUserPost)
-router.post('/addPost', loginRequired, db.addPost)
-router.get('/getAllInfo', loginRequired, db.getAllInfo)
+
+
+
+
+// FEED ROUTES
+// router.get('/getFeed', loginRequired, db.getFeed)
+
+
+
+
+
+// POST ROUTES
+router.post('/newPost', loginRequired, db.newPost)
+router.get('/getUserPost/:id', db.getUserPost)
+
+
+// LIKES ROUTES
+
+
+// router.patch('/updateLikes', loginRequired, db.updateLikes)
+// router.get('/getUserLikes', loginRequired, db.getUserLikes)
+
+
+
+// // FOLLOWERS ROUTES
+router.get('/getFollowers', db.getFollowers)
+router.post('/addFollowers/:id', loginRequired, db.addFollowers)
+
+
+// router.get('/getAllPost', loginRequired, db.getAllPost)
+
+// router.post('/addPost', loginRequired, db.addPost)
+// router.get('/getAllInfo', loginRequired, db.getAllInfo)
 
 
 // router.get('/getFeed', loginRequired, db.getFeed)
